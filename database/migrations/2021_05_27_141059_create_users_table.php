b5f97805b79a8f79d4eb9581a93e5b2b4d8e01ff
yuki-boy <issi0430bjc@gmail.com>
2021-05-28 16:04:06 +0900

    QualificationController作成

--- database/migrations/2021_05_27_141059_create_users_table.php
+++ database/migrations/2021_05_27_141059_create_users_table.php
@@ -17,7 +17,11 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email');
-            $table->timestamps();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
