004158537bca6db2cf3e8677b70be1bb47bbced9
yuki-boy <issi0430bjc@gmail.com>
2021-06-05 21:54:30 +0900

    css修正

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.layout')
 @section('content')
 
-<div class="edit-quali">
+<div class="fills">
   <form method="post" action="{{ route('edit.qualification', ['id' => $quali->id]) }}">
   @csrf
-    <input class="edit_fill" type="text" name="name" value="{{ old('name', $quali->name) }}"><br>
+  <div class="quali_fill">
+    <input class="edit_fill" type="text" name="name" value="{{ old('name', $quali->name) }}">
+  </div>
 
-    取得月<input type="month" name="get_date" value="{{ old('get_date', $quali->get_date) }}"><br>
+  <div class="date_fill">取得月<input type="month" name="get_date" value="{{ old('get_date', $quali->get_date) }}"></div>
 
-    失効月<input type="month" name="lost_date" value="{{ old('lost_date', $quali->lost_date) }}"><br>
+  <div class="date_fill">失効月<input type="month" name="lost_date" value="{{ old('lost_date', $quali->lost_date) }}"></div>
 
+  <div class="edit_buttons">
+    <a href="{{ route('index') }}">
+      <button type="button" class="btn btn-primary">戻る</button>
+    </a>
     <button type="submit" class="btn btn-primary">編集</button>
+  </div>
+
+    <!-- <div class="modal_buttons">
+      <button type="button" id="close" class="btn btn-primary">閉じる</button>
+      <button type="submit" class="btn btn-primary">追加</button>
+    </div> -->
+
+
+
   </form>
 </div>
 
