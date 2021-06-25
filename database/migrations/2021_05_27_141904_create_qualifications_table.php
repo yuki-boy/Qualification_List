9ec4b102e840dba047f50dddb6221ab3b91871d6
yuki-boy <issi0430bjc@gmail.com>
2021-06-25 21:39:54 +0900

    heroku変更

--- database/migrations/2021_05_27_141904_create_qualifications_table.php
+++ database/migrations/2021_05_27_141904_create_qualifications_table.php
@@ -18,8 +18,7 @@ class CreateQualificationsTable extends Migration
             $table->string('name');
             $table->string('get_date')->nullable();
             $table->string('lost_date')->nullable();
-            // $table->integer('sort_num')->nullable(false);
-            $table->integer('sort_num')->default(1);
+            $table->integer('sort_num')->nullable(false);
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
             $table->timestamp('deleted_at')->nullable();
