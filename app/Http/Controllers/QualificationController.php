0e36a222d68028918c64b22083f805a7002a330e
yuki-boy <issi0430bjc@gmail.com>
2021-06-27 07:12:24 +0900

    ソート試し

--- app/Http/Controllers/QualificationController.php
+++ app/Http/Controllers/QualificationController.php
@@ -13,9 +13,9 @@ class QualificationController extends Controller
     {
         $qualis = Qualification::select('id', 'name', 'get_date', 'lost_date', 'sort_num')
             ->where('user_id','=',Auth::id())
-            ->orderBy('qualifications.sort_num')
+            // ->orderBy('qualifications.sort_num')
+            ->increment('qualifications.sort_num')
             ->get();
-        // dd($qualis);
 
         return view('index', compact('qualis'));
     }
